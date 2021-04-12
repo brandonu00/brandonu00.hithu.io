ca58187f7966aab01b039b689e97110bb49e6bbf
brandonu00 <82347960+brandonu00@users.noreply.github.com>
2021-04-12 09:22:54 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,19 +1,10 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/8.3.3/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// @ts-ignore
+firebase.initializeApp({
     apiKey: "AIzaSyDM3eUBIAeBHNzyJXqgiO9x1TvqzdfJA7k",
     authDomain: "brandonu00x.firebaseapp.com",
     projectId: "brandonu00x",
     storageBucket: "brandonu00x.appspot.com",
     messagingSenderId: "1039952814140",
     appId: "1:1039952814140:web:e254d900a04e2fe5376339"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-</script>
+  });
+  
